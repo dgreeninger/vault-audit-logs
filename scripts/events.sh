c2b1c15b9845eb368050f1974c88d84270278dc7
Daniel Greeninger <daniel.greeninger@hashicorp.com>
2024-07-10 09:22:01 -0700

    allows for running the event generator multiple times + cleanup of duplicated login command

--- scripts/events.sh
+++ scripts/events.sh
@@ -9,10 +9,10 @@ echo "generating audit events..."
 export VAULT_ADDR=http://localhost:8200
 export VAULT_TOKEN=$(jq -r .root_token init.json)
 
-# vault auth enable userpass > /dev/null
-# vault auth enable approle > /dev/null
-# vault secrets enable -version=2 kv > /dev/null
-# vault secrets enable database > /dev/null
+vault auth enable userpass > /dev/null || true
+vault auth enable approle > /dev/null || true
+vault secrets enable -version=2 kv > /dev/null || true
+vault secrets enable database > /dev/null || true
 
 for i in {1..20}; do
   vault write auth/userpass/users/$i password=$i > /dev/null
@@ -21,7 +21,6 @@ done
 
 for i in {1..20}; do
   unset VAULT_TOKEN
-  export VAULT_TOKEN=$(vault login -method=userpass -token-only username=$i password=$i > /dev/null 2>&1)
   vault login -method=userpass username=$i password=$i > /dev/null 2>&1
 
   r=$(vault read -field=role_id auth/approle/role/$i/role-id)
@@ -30,7 +29,6 @@ for i in {1..20}; do
 
   vault kv put kv/$i/db db_user=$RANDOM db_password=$RANDOM > /dev/null
   vault kv list kv/$i > /dev/null
-  vault kv put kv/$i/db db_user=$RANDOM db_password=$RANDOM > /dev/null
   vault kv get kv/$i/db > /dev/null
   vault kv metadata delete kv/$i/db > /dev/null
 done
